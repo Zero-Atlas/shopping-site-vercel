0a05c598b1f0ee816156f650fe56d4cbb185ca58
Huỳnh Đắc An <118910962+Zero-Atlas@users.noreply.github.com>
2023-05-25 16:15:20 +0700

    Update Home.js

--- admin/src/pages/Home.js
+++ admin/src/pages/Home.js
@@ -21,7 +21,7 @@ export async function loader() {
   // const response = await fetch(
   //   "https://firebasestorage.googleapis.com/v0/b/funix-subtitle.appspot.com/o/Boutique_products.json?alt=media&token=dc67a5ea-e3e0-479e-9eaf-5e01bcd09c74"
   // );
-  const response = await fetch(`${baseUrl}/trending`, {
+  const response = await fetch(`${baseUrl}/product/trending`, {
     credentials: "include",
     method: "get",
   });
